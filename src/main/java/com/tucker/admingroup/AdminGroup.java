920409a6547e760e886c1cfd518e082e4d4ed7af
yanyumengxi <1211512683@qq.com>
2022-04-13 17:29:49 +0800

    Init Projects

---
+++ src/main/java/com/tucker/admingroup/AdminGroup.java
@@ -0,0 +1,17 @@
+package com.tucker.admingroup;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdminGroup extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
