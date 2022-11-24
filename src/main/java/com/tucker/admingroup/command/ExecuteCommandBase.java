ecb8039d604cb75479212aaf6f151494984006d0
季楠 <103565669+CPJiNan@users.noreply.github.com>
2022-11-24 10:35:58 +0800

    Delete src directory

--- src/main/java/com/tucker/admingroup/command/ExecuteCommandBase.java
+++
@@ -1,8 +0,0 @@
-package com.tucker.admingroup.command;
-
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-
-public interface ExecuteCommandBase {
-    void runCommand(CommandSender sender, Command command, String label, String[] args);
-}
--- src/main/java/com/tucker/admingroup/data/contd/BaseMessageConst.java
+++
@@ -1,5 +0,0 @@
-package com.tucker.admingroup.data.contd;
-
-public enum BaseMessageConst {
-
-}
